86fcbaba180133cacea567f14140d3577058c78f
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-09-03 21:31:11 +0530

    done

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -154,7 +154,7 @@ async function totalExpenseToUser(transactionsWithGroups) {
 }
 
 async function totalAmountGiven(obj) {
-  console.log(obj);
+  // console.log(obj);
   const userId = getUserId();
   const { transactionsWithGroups } = obj;
   const totalAmount = transactionsWithGroups.reduce(
